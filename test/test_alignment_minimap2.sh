1cc1c2475de5e4359a104d77c9d35fd3a79b2c65
Andy Jinseok Lee <andyjinseoklee@gmail.com>
2023-11-22 20:16:26 -0500

    update main.yml

--- test/test_alignment_minimap2.sh
+++ test/test_alignment_minimap2.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
 
-testLongReadMinimap2() {
+runLongReadMinimap2() {
+  TEST_DIR=$1
+
   # Step 1. Define tools available in environment
   NEXTFLOW=nextflow
   MINIMAP2=minimap2
   SAMTOOLS=samtools
 
-  # Step 2. Change directory to current script path
-  SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)/test
-  cd $SCRIPT_DIR
+  # Step 2. Change directory to current test path
+  cd $TEST_DIR
 
   # Step 3. Define nextflow workflow to test
   SCRIPT=../pipelines/alignment/long_read_minimap2/long_read_alignment_minimap2.nf
 
   # Step 4. Create directories
-  WORK_DIR=$SCRIPT_DIR/tmp/test_alignment_minimap2/work
-  INTERMEDIATE=$SCRIPT_DIR/tmp/test_alignment_minimap2/intermediate
-  OUTPUT_DIR=$SCRIPT_DIR/tmp/test_alignment_minimap2/output
+  WORK_DIR=$TEST_DIR/tmp/test_alignment_minimap2/work
+  INTERMEDIATE=$TEST_DIR/tmp/test_alignment_minimap2/intermediate
+  OUTPUT_DIR=$TEST_DIR/tmp/test_alignment_minimap2/output
   mkdir -p $WORK_DIR
   mkdir -p $INTERMEDIATE
   mkdir -p $OUTPUT_DIR
@@ -25,7 +26,7 @@ testLongReadMinimap2() {
   # Step 5. Create samples TSV file
   samples_tsv_file=$INTERMEDIATE/test_alignment_minimap2_samples.tsv
   printf "%s\t%s\n" "sample_id" "fastq_file" > $samples_tsv_file
-  printf "%s\t%s" "sample001" "$SCRIPT_DIR/data/hg38_tp53_variants_dna.fastq.gz" >> $samples_tsv_file
+  printf "%s\t%s" "sample001" "$TEST_DIR/data/hg38_tp53_variants_dna.fastq.gz" >> $samples_tsv_file
 
   # Step 6. Define minimap2 parameters
   MINIMAP2_PARAMS="-ax map-hifi --cs --eqx -Y -L"
@@ -33,9 +34,9 @@ testLongReadMinimap2() {
 
   # Step 7. Run workflow
   nextflow run $SCRIPT -resume \
-    -c $SCRIPT_DIR/nextflow.config \
+    -c $TEST_DIR/nextflow.config \
     --samples_tsv_file $samples_tsv_file \
-    --reference_genome_fasta_file $SCRIPT_DIR/data/hg38_chr17_1-8000000.fa \
+    --reference_genome_fasta_file $TEST_DIR/data/hg38_chr17_1-8000000.fa \
     --minimap2 $MINIMAP2 \
     --minimap2_params "$MINIMAP2_PARAMS_STR" \
     --samtools $SAMTOOLS \
--- unittest.sh
+++ unittest.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
 #wget https://raw.githubusercontent.com/kward/shunit2/master/shunit2
-./shunit2 test/test_alignment_minimap2.sh
-./shunit2 test/test_alignment_minimap2.sh
+
+TEST_DIR=$(cd "$(dirname "$0")" && pwd)/test
+
+. test/test_alignment_minimap2.sh
+
+testLongReadMinimap2() {
+  runLongReadMinimap2 $TEST_DIR
+}
+
+. ./shunit2
