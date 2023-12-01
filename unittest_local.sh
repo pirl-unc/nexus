baf41acf28e9100d19db6594eb2bb30f1d1bf7f2
Andy Jinseok Lee <andyjinseoklee@gmail.com>
2023-12-01 11:38:14 -0500

    test python2

--- unittest_local.sh
+++ unittest_local.sh
@@ -1,8 +1,13 @@
 # Exclude test_paired_end_read_dna_alignment_bwa_mem2.py because abra2 only runs on linux
-# Exclude test_long_read_dna_small_variants_singularity on local machine (test docker)
+# Exclude test_long_read_dna_small_variants_singularity; instead test docker)
+# Exclude test_paired_end_read_dna_somatic_small_variants_human_docker because strelka2 only run on linux
+# Exclude test_paired_end_read_dna_somatic_small_variants_human_singularity because strelka2 only run on linux
 pytest \
   -s \
   --cov-report=term-missing \
   --cov=nexuslib \
   test/ \
-  -k "not test_paired_end_read_dna_alignment_bwa_mem2 and not test_long_read_dna_small_variants_singularity"
+  -k "not test_paired_end_read_dna_alignment_bwa_mem2 and \
+      not test_long_read_dna_small_variants_singularity and \
+      not test_paired_end_read_dna_somatic_small_variants_human_docker and \
+      not test_paired_end_read_dna_somatic_small_variants_human_singularity"
