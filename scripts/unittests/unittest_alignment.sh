9e11c843e96cee7c2ad0a0e61959b8fb7ca7e1b9
Andy Jinseok Lee <andyjinseoklee@gmail.com>
2024-07-22 16:23:20 -0400

    test star

--- scripts/unittests/unittest_alignment.sh
+++ scripts/unittests/unittest_alignment.sh
@@ -3,8 +3,4 @@ pytest \
   --cov-report=term-missing \
   --cov=nexuslib \
   test/ \
-  -k "test_long_read_alignment_minimap2_dna or \
-      test_long_read_alignment_minimap2_rna or \
-      test_long_read_rna_alignment_ultra or \
-      test_paired_end_read_dna_alignment_bwa_mem2 or \
-      test_paired_end_read_rna_alignment_star"
+  -k "test_paired_end_read_rna_alignment_star"
--- scripts/unittests/unittest_variant_calling_dna_long_reads.sh
+++ scripts/unittests/unittest_variant_calling_dna_long_reads.sh
@@ -3,4 +3,11 @@ pytest \
   --cov-report=term-missing \
   --cov=nexuslib \
   test/ \
-  -k "test_long_read_dna_variant_calling_deepvariant_github"
+  -k "test_long_read_dna_variant_calling_cutesv or \
+      test_long_read_dna_variant_calling_deepvariant_github or \
+      test_long_read_dna_variant_calling_pbsv or \
+      test_long_read_dna_variant_calling_savana or \
+      test_long_read_dna_variant_calling_severus or \
+      test_long_read_dna_variant_calling_sniffles2 or \
+      test_long_read_dna_variant_calling_svim or \
+      test_long_read_dna_variant_calling_svisionpro"
