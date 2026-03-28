afa4be63fff4f2c3a91a6c18db8d4bc220512511
Andy Jinseok Lee <andyjinseoklee@gmail.com>
2026-03-27 22:06:53 -0400

    update lint.sh

--- lint.sh
+++ lint.sh
@@ -3,5 +3,5 @@ set -o errexit
 find src test -path test/data -prune -o -name '*.py' -print \
   | xargs pylint \
   --errors-only \
-  --disable=unsubscriptable-object,not-an-iterable,no-member,abstract-class-instantiated
+  --disable=unsubscriptable-object,not-an-iterable,no-member,abstract-class-instantiated,import-error
 echo 'Passes pylint check'
