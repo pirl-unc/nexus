413291632ef235102212531ef00b9287a1e72166
Andy Jinseok Lee <andyjinseoklee@gmail.com>
2026-03-28 08:27:07 -0400

    update subworkflow_assembly.sh

--- scripts/unittests/subworkflow_assembly.sh
+++ scripts/unittests/subworkflow_assembly.sh
@@ -3,6 +3,5 @@ pytest \
   --cov-report=term-missing \
   --cov=nexuslib \
   test/ \
-  -k "test_assembly_hifiasm or \
-      test_assembly_rnabloom2 or \
+  -k "test_assembly_rnabloom2 or \
       test_assembly_stringtie2"
