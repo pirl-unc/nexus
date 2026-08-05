b27462205ea5c91be74369f44289af4e5e610340
Jin Seok (Andy) Lee <andyjinseoklee@gmail.com>
2026-08-05 11:29:26 -0400

    test v0.2.0a8

--- scripts/unittests/workflow_isoform_characterization.sh
+++ scripts/unittests/workflow_isoform_characterization.sh
@@ -1,14 +1,20 @@
 tests=(
-  test_isoform_characterization_long_read
+  test_isoform_characterization_long_read_1
+  test_isoform_characterization_long_read_2
+  test_isoform_characterization_long_read_3
+  test_isoform_characterization_long_read_4
   test_isoform_characterization_short_read
 )
 
-expr=$(printf " or %s" "${tests[@]}")
-expr=${expr:4}
+for test_name in "${tests[@]}"; do
+  pytest \
+    -s \
+    --cov-report=term-missing \
+    --cov=nexuslib \
+    test/ \
+    -k "$test_name"
 
-pytest \
-  -s \
-  --cov-report=term-missing \
-  --cov=nexuslib \
-  test/ \
-  -k "$expr"
+  docker container prune -f || true
+  docker image prune -af || true
+  docker builder prune -af || true
+done
